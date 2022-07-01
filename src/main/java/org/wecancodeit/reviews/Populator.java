3abb5b362bb6cf52f4214360f0dc0c844744af57
Eddie <tastyCanOfMalk@users.noreply.github.com>
2022-06-30 21:46:15 -0400

    added reviews to populator, some addReview() cause problems

--- src/main/java/org/wecancodeit/reviews/Populator.java
+++ src/main/java/org/wecancodeit/reviews/Populator.java
@@ -88,22 +88,80 @@ public class Populator implements CommandLineRunner {
 
         // ADD REVIEWS
         // TODO: possibly change Review:@ManyToOne mapped by behaviour
-        Review review1 = new Review("At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium ", 5, shein);
-        Review review2 = new Review("At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium", 2, shein);
-        Review review3 = new Review("This is a review", 1, shein);
-        Review review4 = new Review("I bought a $2,599 necklace and my wife still left me.", 1, blueNile);
+        Review review1 = new Review("I wish I would have thought of it first. We can't understand how we've been living without Shein. I will refer everyone I know. Thank You!", 5, shein);
+        Review review2 = new Review("I use Shein often.", 2, shein);
+        Review review3 = new Review("Man, this thing is getting better and better as I learn more about it. Thank you for making it painless, pleasant and most of all hassle free!", 4, shein);
+        Review review4 = new Review("I will refer everyone I know. I am really satisfied with my Blue Nile.", 5, blueNile);
+        Review review5 = new Review("Very easy to use. Thanks to Blue Nile, we've just launched our 5th website! Blue Nile should be nominated for service of the year. Really good.", 4, blueNile);
+        Review review6 = new Review("I bought a $2,599 necklace and my wife still left me.", 1, blueNile);
+        Review review7 = new Review("asdfasdfa", 1, dsw);
+        Review review8 = new Review("Great job, I will definitely be ordering again! I would like to personally thank you for your outstanding product.", 4, dsw);
+        Review review9 = new Review("DSW is the most valuable business resource we have EVER purchased.", 2, dsw);
+        Review review10 = new Review("Kohls is the most valuable business resource we have EVER purchased. Absolutely wonderful! Since I invested in Kohls I made over 100,000 dollars profits.", 3, kohls);
+        Review review11 = new Review("Kohls has got everything I need. I will let my mum know about this, she could really make use of Kohls!", 3, kohls);
+        Review review12 = new Review("Keep up the excellent work. Thank you so much for your help. Not able to tell you how happy I am with Kohls.", 4, kohls);
+        Review review13 = new Review("I didn't even need training. Fashion Nova is the most valuable business resource we have EVER purchased.", 4, fashionNova);
+        Review review14 = new Review("Really good.", 5, fashionNova);
+        Review review15 = new Review("This is simply unbelievable!", 4, fashionNova);
+        Review review16 = new Review("Needless to say we are extremely satisfied with the results. Without Shoe Dazzle, we would have gone bankrupt by now. I could probably go into sales for you.", 3, shoeDazzle);
+        Review review17 = new Review("The service was excellent. We've seen amazing results already.", 4, shoeDazzle);
+        Review review18 = new Review("The very best. I will let my mum know about this, she could really make use of Shoe Dazzle! I would like to personally thank you for your outstanding product.", 2, shoeDazzle);
+        Review review19 = new Review("Nike should be nominated for service of the year.", 2, nike);
+        Review review20 = new Review("We have no regrets! You've saved our business! I would gladly pay over 600 dollars for Nike. Nike is the most valuable business resource we have EVER purchased.", 4, nike);
+        Review review21 = new Review("The best on the net!", 5, nike);
+
         shein.addReview(review1);
         shein.addReview(review2);
         shein.addReview(review3);
         blueNile.addReview(review4);
+        blueNile.addReview(review5);
+        blueNile.addReview(review6);
+//        dsw.addReview(review7);
+//        dsw.addReview(review7);
+//        dsw.addReview(review8);
+//        dsw.addReview(review9);
+//        kohls.addReview(review10);
+//        kohls.addReview(review11);
+//        kohls.addReview(review12);
+        fashionNova.addReview(review13);
+        fashionNova.addReview(review14);
+        fashionNova.addReview(review15);
+//        shoeDazzle.addReview(review16);
+//        shoeDazzle.addReview(review17);
+//        shoeDazzle.addReview(review18);
+//        nike.addReview(review19);
+//        nike.addReview(review20);
+//        nike.addReview(review21);
 
         reviewRepo.save(review1);
         reviewRepo.save(review2);
         reviewRepo.save(review3);
         reviewRepo.save(review4);
+        reviewRepo.save(review5);
+        reviewRepo.save(review6);
+        reviewRepo.save(review7);
+        reviewRepo.save(review8);
+        reviewRepo.save(review9);
+        reviewRepo.save(review10);
+        reviewRepo.save(review11);
+        reviewRepo.save(review12);
+        reviewRepo.save(review13);
+        reviewRepo.save(review14);
+        reviewRepo.save(review15);
+        reviewRepo.save(review16);
+        reviewRepo.save(review17);
+        reviewRepo.save(review18);
+        reviewRepo.save(review19);
+        reviewRepo.save(review20);
+        reviewRepo.save(review21);
 
         retailerRepo.save(shein);
         retailerRepo.save(blueNile);
+        retailerRepo.save(dsw);
+        retailerRepo.save(kohls);
+        retailerRepo.save(fashionNova);
+        retailerRepo.save(shoeDazzle);
+        retailerRepo.save(nike);
 
     }
 
